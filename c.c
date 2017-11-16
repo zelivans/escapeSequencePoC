82583123b26f1aecf36a848d8ace798c8f88c5a1
Ariel Zelivansky <ariel@twistlock.com>
2017-11-16 10:14:42 +0200

    Revert to old

--- c.c
+++ c.c
@@ -1,5 +1,4 @@
 #include <fcntl.h>
-#include <string.h>
 
 /* 
  * Twistlock print hack
@@ -7,14 +6,10 @@
  * Also using fraction slash (unicode) because for obvious reasons can't use forward slash in filenames
  */
 
-#define TWISTLOCK "_________         _________ _______ _________ _        _______  _______  _       \n" \
-"\\__   __⁄|\\     ⁄|\\__   __⁄(  ____ \\\\__   __⁄( \\      (  ___  )(  ____ \\| \\    ⁄\\\n" \
-"   ) (   | )   ( |   ) (   | (    \\⁄   ) (   | (      | (   ) || (    \\⁄|  \\  ⁄ ⁄\n" \
-"   | |   | | _ | |   | |   | (_____    | |   | |      | |   | || |      |  (_⁄ ⁄ \n" \
-"   | |   | |( )| |   | |   (_____  )   | |   | |      | |   | || |      |   _ (  \n" \
-"   | |   | || || |   | |         ) |   | |   | |      | |   | || |      |  ( \\ \\ \n" \
-"   | |   | () () |___) (___⁄\\____) |   | |   | (____⁄\\| (___) || (____⁄\\|  ⁄  \\ \\\n" \
-"   )_(   (_______)\\_______⁄\\_______)   )_(   (_______⁄(_______)(_______⁄|_⁄    \\⁄"
+#define TWISTLOCK " _____       _     _   _         _   \n" \
+"|_   _|_ _ _|_|___| |_| |___ ___| |_ \n" \
+"  | | | | | | |_ -|  _| | . |  _| '_|\n" \
+"  |_| |_____|_|___|_| |_|___|___|_,_|"
 
 #define CLEARSCREEN "\e[2J\e[1;1H"
 #define COLORFORMAT "\e[48;5;135;m\e[97m" /* Purple background and white foreground */
@@ -22,14 +17,7 @@
 
 int main()
 {
-        const char twistlock[] = CLEARSCREEN COLORFORMAT TWISTLOCK ENDFORMAT;
-        char next[71] = {0};
-
-        for (int i = 0; i < sizeof(twistlock); i+=70)
-        {
-            strncpy(next, twistlock + i, 70);
-            open(twistlock, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH);
-        }
+        open(CLEARSCREEN COLORFORMAT TWISTLOCK ENDFORMAT, O_RDWR | O_CREAT, S_IRUSR | S_IRGRP | S_IROTH);
 
         return 0;
 }
